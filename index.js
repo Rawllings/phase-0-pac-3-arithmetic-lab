8b93b7603df6c31bd7751a7669e3f2430c487e5f
Rawllings Kiprop <rutojr01@gmail.com>
2022-11-16 19:48:02 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,60 @@
+var a, b
+
+// addition 
+function add() {
+    return a+b;
+}
+//  subtraction 
+function subtract() {
+    return a-b;
+}
+// multiplication 
+function multiply() {
+    return a*b;
+}
+//  division 
+function divide() {
+    return a/b;
+}
+
+// add a & b 
+function add(a, b) {
+    return a + b;
+}
+
+// subtract a & b 
+function subtract(a, b) {
+    return a-b;
+}
+// multiply a & b 
+function multiply(a, b) {
+    return a * b;
+}
+// divide a & b 
+function divide(a, b) {
+    return a/b;
+}
+// increment a 
+function increment(a) {
+    return a + 1;
+
+}
+// decrementing a 
+function decrement(a) {
+    return a - 1;
+}
+// parses/
+function makeInt(a,) {
+    return parseFloat(a, 10);
+}
+
+
+
+function preserveDecimal(n) {
+    return parseInt(n);
+}
+
+function preserveDecimal(a){ 
+    return parseFloat(a);
+}
+
